6fdc357ec4f62ac88b70087bfd004870b30cefe7
ezak <izak.java@gmail.com>
2021-12-01 17:47:40 +0300

    change build to cmake and finished aes_mixcolumn

--- aes-test.c
+++ aes-test.c
@@ -1,62 +1,54 @@
-#include <stdio.h>
 #include "aes.h"
 
 int
 main()
 {
-
-
-  unsigned char k[16] = {
+  /* The initial key that is used to generate all round keys */
+  unsigned char key[16] = {
     0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d,
     0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34
   };
-  
-  unsigned char m[16] = {
+
+  /* The message that we are going to Encrypt */
+  unsigned char msg[16] = {
     0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
     0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
   };
 
+  aes_print(key, "key");
 
-  printf("Key\n");
-  aes_print_4_4_y(k);
-  printf("\n");
-
-  printf("message\n");
-  aes_print_4_4_y(m);
-  printf("\n");
-
-
+  aes_print(msg, "msg");
+  
   for (int i = 0; i < 16; i++)
     {
-      m[i] ^= k[i];
+      msg[i] ^= key[i];
     }
+
+  // printing the begining of the state
+  aes_print(msg, "exp");
+
+
+  // byte by byte substitution
+  aes_subs_bytes(msg);
+
+  // printing the state of the cipher
+  aes_print(msg, "sub");
+
+
+  aes_shift_rows(msg);
   
-  // subs bytes
-  for (int i = 0; i < 4; i++)
-    {
-      for (int j = 0; j < 4; j++)
-	{
-	  aes_subs_byte(m, i, j);
-	}
+  // printing the state of the cipher
+  aes_print(msg, "shf");
 
-    }
-  printf("after substitute bytes\n");
-  aes_print_4_4_y(m);
-  printf("\n");
+  // TODO: comment
+  aes_mix_column(msg, 0);
+  aes_mix_column(msg, 4);
+  aes_mix_column(msg, 8);
+  aes_mix_column(msg, 12);
 
-  aes_shift_by(m, 1, 1);
-  aes_shift_by(m, 1, 2);
-  aes_shift_by(m, 1, 3);
-
-  printf("after shift rows\n");
-  aes_print_4_4_y(m);
-  printf("\n");
-
-  printf("after mix columns\n");
-  aes_mix_column(m);
-  aes_print_4_4_y(m);
-  
-  printf("\n");
+  // printing mix column
+  aes_print(msg, "mix");
   
   return 0;
+  
 }
