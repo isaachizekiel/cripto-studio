b287e06e31166aad0ced70801b1e07f5c06d868f
ezak <izak.java@gmail.com>
2021-12-14 14:27:44 +0300

    clang-format

--- aes-test.c
+++ aes-test.c
@@ -1,54 +1,50 @@
 #include "aes.h"
 
 int
-main()
+main ()
 {
   /* The initial key that is used to generate all round keys */
-  unsigned char key[16] = {
-    0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d,
-    0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34
-  };
+  unsigned char msg[16] = { 0x32, 0x43, 0xf6, 0xa8, 0x88, 0x5a, 0x30, 0x8d,
+                            0x31, 0x31, 0x98, 0xa2, 0xe0, 0x37, 0x07, 0x34 };
 
   /* The message that we are going to Encrypt */
-  unsigned char msg[16] = {
-    0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
-    0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c
-  };
+  unsigned char key[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6,
+                            0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
 
-  aes_print(key, "key");
+  aes_print (key, "key");
 
-  aes_print(msg, "msg");
-  
+  aes_print (msg, "msg");
+
   for (int i = 0; i < 16; i++)
     {
       msg[i] ^= key[i];
     }
 
   // printing the begining of the state
-  aes_print(msg, "exp");
-
+  aes_print (msg, "exp");
 
   // byte by byte substitution
-  aes_subs_bytes(msg);
+  aes_subs_bytes (msg);
 
   // printing the state of the cipher
-  aes_print(msg, "sub");
+  aes_print (msg, "sub");
 
+  aes_shift_rows (msg);
 
-  aes_shift_rows(msg);
-  
   // printing the state of the cipher
-  aes_print(msg, "shf");
+  aes_print (msg, "shf");
 
   // TODO: comment
-  aes_mix_column(msg, 0);
-  aes_mix_column(msg, 4);
-  aes_mix_column(msg, 8);
-  aes_mix_column(msg, 12);
+  aes_mix_column (msg, 0);
+  aes_mix_column (msg, 4);
+  aes_mix_column (msg, 8);
+  aes_mix_column (msg, 12);
 
   // printing mix column
-  aes_print(msg, "mix");
-  
+  aes_print (msg, "mix");
+
+  //
+  aes_key_schedule (key);
+
   return 0;
-  
 }
